07662475228d7382090557d42f16186ffa18cbd6
Gabriel Nunes <gabriel@multiverso.me>
2019-02-10 13:57:48 -0200

    Translate Home Page

--- content/home/examples/a-simple-component.js
+++ content/home/examples/a-simple-component.js
@@ -2,13 +2,13 @@ class HelloMessage extends React.Component {
   render() {
     return (
       <div>
-        Hello {this.props.name}
+        Olá, {this.props.name}!
       </div>
     );
   }
 }
 
 ReactDOM.render(
-  <HelloMessage name="Taylor" />,
+  <HelloMessage name="Daniel" />,
   document.getElementById('hello-example')
 );
