8dd7e359f10e5dab9d9fb0151a95bf5d84d26792
Jhon Mike <developer@jhonmike.com.br>
2019-05-19 14:49:19 -0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -50,13 +50,8 @@ class Home extends Component {
     return (
       <Layout location={location}>
         <TitleAndMetaTags
-<<<<<<< HEAD
           title="React &ndash; Uma biblioteca JavaScript para criar interfaces de usuário"
-          ogUrl={createOgUrl('index.html')}
-=======
-          title="React &ndash; A JavaScript library for building user interfaces"
           canonicalUrl={createCanonicalUrl('/')}
->>>>>>> 06deefa56756f8dac13af59ee657186a81d28151
         />
         <div css={{width: '100%'}}>
           <header
