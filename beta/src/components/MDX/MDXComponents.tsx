cb4a477581e855f491901505f9be3521cd7b6717
André Tashiro <andre_seichi@hotmail.com>
2023-01-24 22:46:23 -0300

    tradução do título do componente YouWillLearn (#600)
    Co-authored-by: Jhon Mike <jhon.msdev@gmail.com>

--- beta/src/components/MDX/MDXComponents.tsx
+++ beta/src/components/MDX/MDXComponents.tsx
@@ -164,7 +164,7 @@ function YouWillLearn({
   children: any;
   isChapter?: boolean;
 }) {
-  let title = isChapter ? 'In this chapter' : 'You will learn';
+  let title = isChapter ? 'Neste capítulo' : 'Você aprenderá';
   return <SimpleCallout title={title}>{children}</SimpleCallout>;
 }
 
